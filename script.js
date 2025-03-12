ab27db05b4a8c77928e5866893c1885afef3d4d9
Nikhil293 <nikhil29300@gmail.com>
2025-03-12 19:44:32 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,55 +1,118 @@
-var timer = 6;
-var score = 0;
-var hitrn = 0;
 
-function getNewHit(){
-    hitrn = Math.floor(Math.random()*10);
-    document.querySelector("#hitval").innerHTML = hitrn;
+
+
+function mouseScale(){
+    var xscale = 1;
+    var yscale = 1;
+    var xprev = 0;   
+    var yprev = 0;   
+
+    window.addEventListener("mousemove", function(dets){
+        clearTimeout(timeout);
+        var xdiff = dets.clientX - xprev;
+        var ydiff = dets.clientY - yprev;
+        xscale = gsap.utils.clamp(0.7, 1.3, xdiff);
+        yscale = gsap.utils.clamp(0.7, 1.3, ydiff);
+        
+        xprev = dets.clientX;
+        yprev = dets.clientY;
+        
+        circleMouse(xscale, yscale);
+
+        var timeout = setTimeout(function(){
+            document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`;
+   
+        }, 100);
+    });
 }
 
-function scoreAdd(){
- score += 10;
- document.querySelector("#scoreval").innerHTML = score;
+function circleMouse(xscale, yscale){
+   window.addEventListener("mousemove", function(dets){
+        // mouse scale ke pehle, document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
 
+        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale},${yscale})`;
+ 
+    })
 }
 
-function makeBubble(){ 
+function pageOneAnime(){
+    var tl = gsap.timeline();
+
+    tl.from("nav a",{
+        opacity:0,
+        duration:0.8,
+        stagger:0.2
+    })
+    tl.to(".tt",{
+        y:0,
+        stagger:0.2
+    })
     
-    let clutter='';
-
-    for(let i=1; i<=208; i++){
-     var rn =Math.floor(Math.random()*10);
-     clutter += `<div class="bubble">${rn}</div>`;
-     
-    }
-    document.querySelector(".btm").innerHTML = clutter;
 }
 
-function runTimer(){
-  var timerr = setInterval(function(){
-    if(timer > 0){
-        timer--;
-        document.querySelector("#timval").textContent = timer;
-    }
-    else{
-        clearInterval(timerr);
-        // document.querySelector(".btm").textContent = '';
-        document.querySelector(".btm").innerHTML = `<h1>Game Over</h1>`;
-    }
-   },1000);
-}
+mouseScale();
+pageOneAnime();
+circleMouse();
 
-document.querySelector(".btm").addEventListener("click", function(dets){
- var clickedNum = Number(dets.target.textContent);
- if(clickedNum === hitrn){
-    scoreAdd();
-    makeBubble();
-    getNewHit();
- }
+document.querySelectorAll(".elem").forEach(function(elem){
+    var rotate = 0;
+    var diffrot = 0;
+    elem.addEventListener("mouseleave", function(dets){
+
+        
+        gsap.to(elem.querySelector("img"),{
+            opacity: 0,
+            ease: Power1,
+            
+        })
+    })
+    elem.addEventListener("mousemove", function(dets){
+
+        var diff = dets.clientY - elem.getBoundingClientRect().top;
+        diffrot = dets.clientX - rotate;
+        rotate = dets.clientX;
+        
+        gsap.to(elem.querySelector("img"),{
+            opacity: 1,
+            ease: Power1,
+            top: diff,
+            left: dets.clientX,
+            rotate:gsap.utils.clamp(-20,20, diffrot),
+        })
+    })
 })
 
 
+// gsap.to(".text",{
+//     scrollTrigger:{
+//         trigger:".pg3",
+//         pin:true,
+//         start:"top top",
+//         end:"bottom bottom",
+//         endTrigger:"#last",
+//         markers:true,
+//         scrub:2,
+//     },
+//     y:"-300%",
+// })
 
-runTimer();
-makeBubble();
-getNewHit();
+var tl = gsap.timeline({
+    scrollTrigger:{
+        trigger:".pg3",
+        pin:true,
+        start:"top top",
+        end:"bottom bottom",
+        endTrigger:"#last",
+        markers:true,
+        scrub:2,
+}});
+tl
+.to(".text",{
+    y:"-300%",
+
+},'a')
+
+.to(".images",{
+    y:"-300%",
+    duration:0.8
+},'a')
